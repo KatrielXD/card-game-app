cc5353b0fd0c0096b66d2fbc0c6eb898bc4acae1
KatrielXD <katrieldiaz@gmail.com>
2025-01-10 19:56:33 -0300

    Update backend URL

--- src/app/services/card-game.service.ts
+++ src/app/services/card-game.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class CardGameService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'https://card-game-backend-tj7j.onrender.com';
 
   constructor(private http: HttpClient) {}
 
